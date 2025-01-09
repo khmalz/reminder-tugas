bad24ccd81a77303eae4e05390c892c92cdbc9d3
khmalz <akmalrma1710@gmail.com>
2025-01-09 13:09:21 +0700

    using FutureBuilder instead load onInit on Home page

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -2,22 +2,17 @@ import 'dart:convert';
 
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:get/get.dart';
+import 'package:reminder_tugas/app/data/models/task_model.dart';
 
 class HomeController extends GetxController {
-  var tugasList = <Map<String, dynamic>>[].obs;
-
-  @override
-  void onInit() {
-    super.onInit();
-    loadTugas();
-  }
-
-  Future<void> loadTugas() async {
+  Future<List<Task>> loadTugas() async {
     String jsonString = await rootBundle.loadString('assets/data/tugas.json');
 
     Map<String, dynamic> jsonResponse = json.decode(jsonString);
     List<dynamic> tugasData = jsonResponse['tugas'];
 
-    tugasList.value = tugasData.cast<Map<String, dynamic>>();
+    List<Task> tasks = tugasData.map((task) => Task.fromJson(task)).toList();
+
+    return tasks;
   }
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -19,104 +19,120 @@ class HomeView extends GetView<HomeController> {
         centerTitle: true,
         foregroundColor: textPrimary,
       ),
-      body: ListView(
-        children: [
-          Container(
-            height: 70,
-            decoration: BoxDecoration(
-              color: textPrimary,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.withOpacity(0.5),
-                  spreadRadius: 2,
-                  blurRadius: 3,
-                  offset: const Offset(0, 3), // changes position of shadow
-                ),
-              ],
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text(
-                      'Telat',
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: textSecondary,
-                      ),
+      body: FutureBuilder(
+          future: controller.loadTugas(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                  child: CircularProgressIndicator()); // Menunggu data
+            } else if (snapshot.hasError) {
+              return Center(child: Text('Error: ${snapshot.error}'));
+            } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+              return Center(child: Text('Tidak ada tugas ditemukan.'));
+            } else {
+              var tugasList = snapshot.data!;
+
+              return ListView(
+                children: [
+                  Container(
+                    height: 70,
+                    decoration: BoxDecoration(
+                      color: textPrimary,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 2,
+                          blurRadius: 3,
+                          offset:
+                              const Offset(0, 3), // changes position of shadow
+                        ),
+                      ],
                     ),
-                    Text(
-                      '10',
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.red,
-                      ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text(
+                              'Telat',
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: textSecondary,
+                              ),
+                            ),
+                            Text(
+                              '10',
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.red,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text(
+                              'Belum',
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: textSecondary,
+                              ),
+                            ),
+                            Text(
+                              '10',
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.amber.shade700,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text(
+                              'Sudah',
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: textSecondary,
+                              ),
+                            ),
+                            Text(
+                              '10',
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.green,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text(
-                      'Belum',
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: textSecondary,
-                      ),
-                    ),
-                    Text(
-                      '10',
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.amber.shade700,
-                      ),
-                    ),
-                  ],
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text(
-                      'Sudah',
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: textSecondary,
-                      ),
-                    ),
-                    Text(
-                      '10',
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.green,
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-          Column(
-            children: controller.tugasList
-                .map((tugas) => ListTugas(
-                      id: tugas['id'].toString(),
-                      namaTugas: tugas['name'],
-                      matkul: tugas['matkul'],
-                      tipe: tugas['type'],
-                      pengumpulan: tugas['collection'],
-                      deadline: tugas['deadline'],
-                    ))
-                .toList(),
-          )
-        ],
-      ),
+                  ),
+                  Column(
+                    children: tugasList
+                        .map((tugas) => ListTugas(
+                              id: tugas.id.toString(),
+                              namaTugas: tugas.name!,
+                              matkul: tugas.matkul!,
+                              tipe: tugas.type!,
+                              pengumpulan: tugas.collection!,
+                              deadline: tugas.deadline!,
+                            ))
+                        .toList(),
+                  ),
+                ],
+              );
+            }
+          }),
       floatingActionButton: SizedBox(
         width: 70,
         height: 70,
